9732c71b1b166b9e6b9de61f58e250dad1c28379
Racabane <Racabanellas2018@gmail.com>
2025-10-29 19:40:59 -0400

    lab 6

--- Lab06/Project4/main.cpp
+++ Lab06/Project4/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
-#include <list>
+#include <vector>
 
-int allBasketsminsadded(std::list<int> numbers){
+
+int allBasketsminsadded(std::vector<int> numbers){
+	//sort the input
+	for(int i = 0; i < numbers.size() - 1; i++){
+		for(int j = 0; j < numbers.size() - i - 1; j++){
+			if(numbers[j] > numbers[j + 1]){
+				int temp = numbers[j + 1];
+				numbers[j + 1] = numbers[j];
+				numbers[j] = temp;
+			}
+		}
+	}
+
+	// for(int k = 0; k < numbers.size(); k ++){
+	// 	std::cout << numbers[k] << " ";
+	// }
 	
+	int total = 0;
+	//add the smallest of the pair
+	for(int i = 0; i < numbers.size(); i += 2){
+		total += numbers[i];
+	}
+	return total;
 }
 
 int main(){
-	std::list<int> numbers{7,3,5,9};
-	allBasketsminsadded(numbers);
+	std::vector<int> numbers{7,3,5,9};
+	std::cout << allBasketsminsadded(numbers) << "\n";
 }
